eb9ebe294aa95cb89b3bd2eb590fd6e2f363b96e
zz <runzhi.zzhang@gmail.com>
2024-11-12 18:02:18 +0700

    work later

--- pipex/src/utils.c
+++ pipex/src/utils.c
@@ -6,7 +6,7 @@
 /*   By: ruzhang <ruzhang@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/12 10:18:44 by ruzhang           #+#    #+#             */
-/*   Updated: 2024/11/12 17:22:59 by ruzhang          ###   ########.fr       */
+/*   Updated: 2024/11/12 17:55:15 by ruzhang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -56,18 +56,50 @@ char	**parse_path(char **envp)
 	return (paths);
 }
 
-void	check_execute(char *path, char **args, char **envp)
+void	check_execute(char *path, char **args, char **envp, char **paths)
 {
 	if (access(path, F_OK) == 0)
 	{
 		if (access(path, X_OK) == -1)
+		{
+			free_arr(paths);
+			free_arr(args);
 			ft_error("Command no permission", 126);
+		}
 		if (execve(path, args, envp) == -1)
 		{
+			free_arr(paths);
 			free_arr(args);
 			ft_error("execve failed", 1);
 		}
 	}
+}
+
+
+char	*join_str(char const *s1, char const *s2)
+{
+	char	*str;
+	char	*tmp;
+
+	if (!s1 && !s2)
+		return (NULL);
+	str = malloc(ft_strlen(s1) + ft_strlen(s2) + 1 + 1);
+	if (!str)
+		return (0);
+	tmp = str;
+	if (s1)
+	{
+		while (*s1)
+			*tmp++ = *s1++;
+	}
+	*tmp++ = '/';
+	if (s2)
+	{
+		while (*s2)
+			*tmp++ = *s2++;
+	}
+	*tmp = '\0';
+	return (str);
 }
 
 void	execute(char *cmd, char **envp)
@@ -75,21 +107,21 @@ void	execute(char *cmd, char **envp)
 	char	**args;
 	char	**paths;
 	char	*full;
-	char	*tmp;
 	int		i;
 
 	args = ft_split(cmd, ' ');
 	if ((!args || !*args) && (access("", F_OK) == -1))
-		return (free_arr(args), ft_error("Command not found", 127));
-	check_execute(args[0], args, envp);
+	{
+		free_arr(args);
+		ft_error("Command not found", 127);
+	}
+	check_execute(args[0], args, envp, NULL);
 	paths = parse_path(envp);
 	i = 0;
 	while (paths[i])
 	{
-		tmp = ft_strjoin("/", args[0]);
-		full = ft_strjoin(paths[i], tmp);
-		free(tmp);
-		check_execute(full, args, envp);
+		full = join_str(paths[i], args[0]);
+		check_execute(full, args, envp, paths);
 		free(full);
 		i++;
 	}
