13c69a0feaf2bfff3b5502906e1a7e8bd01c3cd8
zz <runzhi.zzhang@gmail.com>
2024-12-04 12:08:21 +0700

    finish ex02

--- cpp/cpp00/ex02/Account.cpp
+++ cpp/cpp00/ex02/Account.cpp
@@ -6,16 +6,20 @@
 /*   By: ruzhang <ruzhang@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/29 17:25:25 by ruzhang           #+#    #+#             */
-/*   Updated: 2024/12/01 15:53:41 by ruzhang          ###   ########.fr       */
+/*   Updated: 2024/12/04 12:00:36 by ruzhang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Account.hpp"
 #include <iostream>
+#include <iomanip>
 
 int	Account::_nbAccounts = 0;
+
 int	Account::_totalAmount = 0;
+
 int	Account::_totalNbDeposits = 0;
+
 int	Account::_totalNbWithdrawals = 0;
 
 Account::Account( int initial_deposit )
@@ -63,9 +67,30 @@ int		Account::checkAmount( void ) const
 	return _amount;
 }
 
+/*
+struct tm {
+    int tm_sec;   // Seconds (0 to 59)
+    int tm_min;   // Minutes (0 to 59)
+    int tm_hour;  // Hours (0 to 23)
+    int tm_mday;  // Day of the month (1 to 31)
+    int tm_mon;   // Month (0 to 11; January is 0)
+    int tm_year;  // Year since 1900 (e.g., 2024 would be 124)
+    int tm_wday;  // Day of the week (0 to 6; Sunday is 0)
+    int tm_yday;  // Day of the year (0 to 365)
+    int tm_isdst; // Daylight saving time flag (positive if DST, 0 if not, negative if unknown)
+};
+*/
 void	Account::_displayTimestamp( void )
 {
-	
+	time_t	timestamp = time(NULL);
+	tm * tm = localtime(&timestamp);
+
+	std::cout << "[" << (tm->tm_year + 1900);
+	std::cout << std::setfill('0') << std::setw(2) << (tm->tm_mon + 1);
+	std::cout << std::setfill('0') << std::setw(2) << tm->tm_mday << "_";
+	std::cout << std::setfill('0') << std::setw(2) << tm->tm_hour;
+	std::cout << std::setfill('0') << std::setw(2) << tm->tm_min;
+	std::cout << std::setfill('0') << std::setw(2) << tm->tm_sec << "] ";
 }
 
 void	Account::displayAccountsInfos( void )
@@ -73,14 +98,14 @@ void	Account::displayAccountsInfos( void )
 	Account::_displayTimestamp();
 	std::cout << "accounts:" << Account::getNbAccounts() << ";total:" \
 				<< Account::getTotalAmount() << ";deposits:" << Account::getNbDeposits() \
-				<< ";withdrawls:" << Account::getNbWithdrawals() << std::endl;
+				<< ";withdrawals:" << Account::getNbWithdrawals() << std::endl;
 }
 
 void	Account::displayStatus( void ) const
 {
 	Account::_displayTimestamp();
 	std::cout << "index:" << _accountIndex << ";amount:" << checkAmount() \
-		<< ";deposits:" << _nbDeposits << ";withdrawls:" << _nbWithdrawals << std::endl;
+		<< ";deposits:" << _nbDeposits << ";withdrawals:" << _nbWithdrawals << std::endl;
 }
 
 void	Account::makeDeposit( int deposit )
@@ -105,7 +130,7 @@ bool	Account::makeWithdrawal( int withdrawal )
 
 	if (withdrawal > checkAmount())
 	{
-		std::cout << ";withdrawl:refused\n";
+		std::cout << ";withdrawal:refused\n";
 		return false;
 	}
 
@@ -115,8 +140,7 @@ bool	Account::makeWithdrawal( int withdrawal )
 	Account::_totalAmount -= withdrawal;
 	Account::_totalNbWithdrawals++;
 
-	_displayTimestamp();
-	std::cout << ";withdrawl:" << withdrawal << ";amount:" << checkAmount() \
-				<< ";nb_withdrawls:" << _nbWithdrawals << std::endl;
+	std::cout << ";withdrawal:" << withdrawal << ";amount:" << checkAmount() \
+				<< ";nb_withdrawals:" << _nbWithdrawals << std::endl;
 	return true;
 }
