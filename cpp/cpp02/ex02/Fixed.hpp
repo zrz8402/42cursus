c70f58d1b882fb6b2c54388acbc733e52f41ef56
zz <runzhi.zzhang@gmail.com>
2025-01-08 14:33:22 +0700

    add file

---
+++ cpp/cpp02/ex02/Fixed.hpp
@@ -0,0 +1,65 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ruzhang <ruzhang@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/04 12:10:19 by ruzhang           #+#    #+#             */
+/*   Updated: 2025/01/08 14:22:10 by ruzhang          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef __FIXED_HPP__
+#define __FIXED_HPP__
+
+#include <iostream>
+
+class Fixed {
+
+private:
+	int					value;
+	static const int	FRACTIONAL_BITS = 8;
+
+public:
+	Fixed();
+	Fixed( const int value );
+	Fixed& operator=(const Fixed &other );
+	~Fixed();
+	
+	Fixed( const float value );
+	Fixed( const Fixed &other );
+	
+	int		getRawBits( void ) const;
+	void	setRawBits( int const raw );
+	float	toFloat( void ) const;
+	int		toInt( void ) const;
+
+	bool operator<(const Fixed &other ) const;
+	bool operator<=(const Fixed &other ) const;
+	bool operator>(const Fixed &other ) const;
+	bool operator>=(const Fixed &other ) const;
+	bool operator==(const Fixed &other ) const;
+	bool operator!=(const Fixed &other ) const;
+
+	Fixed operator+(const Fixed &other ) const ;
+	Fixed operator-(const Fixed &other ) const ;
+	Fixed operator*(const Fixed &other ) const ;
+	Fixed operator/(const Fixed &other ) const ;
+
+	// pre-increment ++a
+	Fixed& operator++();
+	// post-increment a++
+	Fixed operator++(int);
+	Fixed& operator--();
+	Fixed operator--(int);
+
+	static Fixed& min(Fixed &a, Fixed &b);
+	static const Fixed& min(const Fixed &a, const Fixed &b);
+	static Fixed& max(Fixed &a, Fixed &b);
+	static const Fixed& max(const Fixed &a, const Fixed &b);
+};
+
+std::ostream& operator<<(std::ostream& os, const Fixed &obj);
+
+#endif
---
+++ cpp/cpp02/ex02/main.cpp
@@ -0,0 +1,66 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ruzhang <ruzhang@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/08 11:40:39 by ruzhang           #+#    #+#             */
+/*   Updated: 2025/01/08 14:32:58 by ruzhang          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include "Fixed.hpp"
+
+int main( void ) {
+	Fixed a;
+
+	std::cout << "original a: " << a << std::endl; // 0
+
+	std::cout << "\npre-/post-increment" << std::endl;
+	std::cout << "++a: "<< ++a << std::endl; // 0.00390625
+	std::cout << "a after ++a: "<< a << std::endl; // 0.00390625
+	std::cout << "a++: "<< a++ << std::endl; // 0.00390625
+	std::cout << "a after a++: "<< a << std::endl; // 0.0078125
+
+	std::cout << "\npre-/post-decrement" << std::endl;
+	std::cout << "--a: "<< --a << std::endl;
+	std::cout << "a after --a: "<< a << std::endl;
+	std::cout << "a--: "<< a-- << std::endl;
+	std::cout << "a after a--: "<< a << std::endl;
+
+	std::cout << std::endl;
+	Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
+	std::cout << "b: " << b << std::endl; // 10.1016
+
+	std::cout << "\nmin & max" << std::endl;
+	std::cout << Fixed::max( a, b ) << std::endl;
+	// const Fixed x1(3);
+	// const Fixed y1(4);
+	// std::cout << "const: " << Fixed::min(x1, y1) << std::endl;
+	// Fixed x2(3);
+	// Fixed y2(4);
+	// std::cout << "not const: " << Fixed::min(x2, y2) << std::endl;
+
+	// std::cout << "\ncomparison operators" << std::endl;
+	// std::cout << (a > b) << std::endl;
+	// std::cout << (a <= b) << std::endl;
+	// std::cout << (a == b) << std::endl;
+	// std::cout << (a != b) << std::endl;
+
+	// std::cout << "\narithmetic operators" << std::endl;
+	// Fixed const c( Fixed( 5.05f ) + Fixed( 2 ) );
+	// std::cout << c << std::endl; // 7.05078
+
+	// // divided by zero
+	// Fixed const d( Fixed( 5.05f ) / Fixed( 0 ) );
+	// std::cout << d << std::endl;
+
+	return 0;
+}
+
+// Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
+// Fixed(5.05f) -> 5.05 * 256 = 1292.8 ->roundf -> 1293
+// Fixed( 2 ) -> 2 * 256 = 512
+// 1293 / 256 * 2 / 256 = 10.1015625 -> close to 10.1016
