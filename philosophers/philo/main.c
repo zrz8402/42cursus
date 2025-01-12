200b6813b09a75ee67e3452d59eaefac60b5e0ad
zz <runzhi.zzhang@gmail.com>
2025-01-12 16:07:00 +0700

    update

--- philosophers/philo/main.c
+++ philosophers/philo/main.c
@@ -1,24 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   philo.h                                            :+:      :+:    :+:   */
+/*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ruzhang <ruzhang@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/01/12 12:58:22 by ruzhang           #+#    #+#             */
-/*   Updated: 2025/01/12 16:05:54 by ruzhang          ###   ########.fr       */
+/*   Created: 2025/01/12 15:52:21 by ruzhang           #+#    #+#             */
+/*   Updated: 2025/01/12 16:06:01 by ruzhang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PHILO_H
-# define PHILO_H
+#include "philo.h"
 
-# include <pthread.h>
-# include <stdlib.h>
-# include <stdio.h>
-# include <unistd.h>
-
-# define MAX_PHILO 200
-
-int	check_input(char **av);
-#endif
+int	main(int ac, char **av)
+{
+	if ((ac != 5 && ac != 6) || check_input(av) == -1)
+	{
+		printf("Usage: ./philo number_of_philosophers time_to_die time_to_eat time_to_sleep [number_of_times_each_philosopher_must_eat]");
+		return (0);
+	}
+}
