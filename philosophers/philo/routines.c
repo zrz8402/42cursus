7202459046873343afc1aef024f2f79a118396a9
zz <runzhi.zzhang@gmail.com>
2025-02-01 15:57:10 +0700

    some problem

--- philosophers/philo/routines.c
+++ philosophers/philo/routines.c
@@ -6,7 +6,7 @@
 /*   By: ruzhang <ruzhang@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/15 15:07:57 by ruzhang           #+#    #+#             */
-/*   Updated: 2025/02/01 14:45:26 by ruzhang          ###   ########.fr       */
+/*   Updated: 2025/02/01 15:56:35 by ruzhang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,26 +14,34 @@
 
 void	ft_think(t_philo *philo, t_table *table)
 {
-	if (philo->id % 2 == 0)
-		ft_usleep(1);
+	// if (philo->id % 2 == 0)
+	// 	ft_usleep(1);
 	write_message("is thinking", philo, table);
 }
 
 void	ft_sleep(t_philo *philo, t_table *table)
 {
-	// pthread_mutex_lock(&table->stop_lock);
-	// if (table->stop)
-	// {
-	// 	pthread_mutex_unlock(&table->stop_lock);
-	// 	return ;
-	// }
-	// pthread_mutex_unlock(&table->stop_lock);
+	pthread_mutex_lock(&table->stop_lock);
+	if (table->stop)
+	{
+		pthread_mutex_unlock(&table->stop_lock);
+		return ;
+	}
+	pthread_mutex_unlock(&table->stop_lock);
 	write_message("is sleeping", philo, table);
 	ft_usleep(table->time_to_sleep);
 }
 
 void ft_eat(t_philo *philo, t_table *table)
 {
+	pthread_mutex_lock(&table->stop_lock);
+	if (table->stop)
+	{
+		pthread_mutex_unlock(&table->stop_lock);
+		return ;
+	}
+	pthread_mutex_unlock(&table->stop_lock);
+
     if (philo->id % 2 == 0) {
         pthread_mutex_lock(philo->l_fork);
         write_message("has taken a fork", philo, table);
@@ -46,18 +54,6 @@ void ft_eat(t_philo *philo, t_table *table)
         write_message("has taken a fork", philo, table);
     }
 
-    // pthread_mutex_lock(philo->r_fork);
-    // write_message("has taken a fork", philo, table);
-
-    // if (table->num_philos == 1) {
-    //     ft_usleep(table->time_to_die);
-    //     pthread_mutex_unlock(philo->r_fork);
-    //     return ;
-    // }
-
-    // pthread_mutex_lock(philo->l_fork);
-    // write_message("has taken a fork", philo, table);
-
     write_message("is eating", philo, table);
     pthread_mutex_lock(&philo->time_lock);
     philo->last_meal = get_current_time();
@@ -69,20 +65,28 @@ void ft_eat(t_philo *philo, t_table *table)
     philo->meals_eaten++;
     pthread_mutex_unlock(&philo->meal_lock);
 
-    // Unlock forks
     pthread_mutex_unlock(philo->l_fork);
     pthread_mutex_unlock(philo->r_fork);
 
-    // Check if all philosophers have finished eating
     if (table->num_times_must_eat != -1 && philo->meals_eaten >= table->num_times_must_eat) {
-        pthread_mutex_lock(&table->finish_lock);
+        pthread_mutex_lock(&table->stop_lock);
         table->num_finished++;
         if (table->num_finished == table->num_philos) {
-            pthread_mutex_lock(&table->stop_lock);
             table->stop = 1;
-            pthread_mutex_unlock(&table->stop_lock);
         }
-        pthread_mutex_unlock(&table->finish_lock);
+        pthread_mutex_unlock(&table->stop_lock);
+    }
+}
+void	single_philo(t_philo *philo, t_table *table)
+{
+    if (table->num_philos == 1) {
+		pthread_mutex_lock(philo->r_fork);
+		write_message("has taken a fork", philo, table);
+        ft_usleep(table->time_to_die);
+        pthread_mutex_unlock(philo->r_fork);
+		pthread_mutex_lock(&table->stop_lock);
+		table->stop = 1;
+		pthread_mutex_unlock(&table->stop_lock);
     }
 }
 
@@ -95,6 +99,7 @@ void	*routine(void *arg)
 	table = philo->table;
 	while (1)
 	{
+		// single_philo(philo, table);
 		pthread_mutex_lock(&table->stop_lock);
 		if (table->stop || table->num_finished >= table->num_philos)
 		{
