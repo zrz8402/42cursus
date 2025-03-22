57bf80ad63bd18473f0374c7f6f94a33534df5b7
zz <runzhi.zzhang@gmail.com>
2025-03-22 15:48:18 +0700

    add Makefile and MLX lib

---
+++ miniRT/inc/minirt.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minirt.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ruzhang <ruzhang@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/22 15:23:00 by ruzhang           #+#    #+#             */
+/*   Updated: 2025/03/22 15:32:28 by ruzhang          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINIRT_H
+# define MINIRT_H
+
+#define WIDTH 256
+#define HEIGHT 256
+
+# include "include/MLX42/MLX42.h"
+# include <math.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <fcntl.h>
+
+
+#endif
