572795efb94073b80e25d62a22de730ba22b12a0
zz <runzhi.zzhang@gmail.com>
2025-03-23 14:39:25 +0700

    add notes: mlx42 functions

--- miniRT/inc/minirt.h
+++ miniRT/inc/minirt.h
@@ -6,15 +6,15 @@
 /*   By: ruzhang <ruzhang@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/22 15:23:00 by ruzhang           #+#    #+#             */
-/*   Updated: 2025/03/22 15:32:28 by ruzhang          ###   ########.fr       */
+/*   Updated: 2025/03/23 14:03:21 by ruzhang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINIRT_H
 # define MINIRT_H
 
-#define WIDTH 256
-#define HEIGHT 256
+#define WIDTH 1280
+#define HEIGHT 720
 
 # include "include/MLX42/MLX42.h"
 # include <math.h>
@@ -23,5 +23,10 @@
 # include <unistd.h>
 # include <fcntl.h>
 
+typedef struct s_rt
+{
+	mlx_t		*mlx;
+	mlx_image_t	*img;
+}	t_rt;
 
 #endif
--- miniRT/src/main.c
+++ miniRT/src/main.c
@@ -6,8 +6,32 @@
 /*   By: ruzhang <ruzhang@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/22 15:22:48 by ruzhang           #+#    #+#             */
-/*   Updated: 2025/03/22 15:44:14 by ruzhang          ###   ########.fr       */
+/*   Updated: 2025/03/23 14:27:14 by ruzhang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minirt.h"
+
+void	init_rt(t_rt *rt)
+{
+	rt->mlx = mlx_init(WIDTH, HEIGHT, "", false);
+	if (rt->mlx)
+	{
+		perror("Error initiating mlx\n");
+		return ;
+	}
+	rt->img = mlx_new_image(rt->mlx, WIDTH, HEIGHT);
+	if (rt->img)
+	{
+		mlx_terminate(rt->mlx);
+		perror("Error creating image\n");
+		return ;
+	}
+}
+
+int	main(void)
+{
+	t_rt	rt;
+
+	init_rt(&rt);
+}
